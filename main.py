f03b7579facb3627fdf9713956110a0ccf4b440c
joaogoncalvesalves276-png <joaogoncalvesalves276@gmail.com>
2026-09-02 16:04:20 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-async def main():
-    await client.connect()
-    if not await client.is_user_authorized():
-        # Deixamos sem o 'code' para forçar o Telegram a gerar e enviar um novo para você
-        await client.send_code_request('+5574981222350')
-        print("⚠️ Código solicitado! Verifique seu app do Telegram.")
-        await asyncio.sleep(300) # Mantém o servidor esperando você pegar o código
-    else:
-        print("✅ Autenticação realizada com sucesso!")
-        await executar_envios()
-        
+await client.start(phone='+5574981222350')
