63a193293852e0c30428a6e63022d0ceba7b9ae5
joaogoncalvesalves276-png <joaogoncalvesalves276@gmail.com>
2026-09-02 16:24:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import asyncio
 from telethon import TelegramClient
+from telethon.errors import SessionPasswordNeededError
 
 api_id = 26553805
 api_hash = '8ec7d031bf3bb7904ba01e149ef7b629'
@@ -9,9 +10,10 @@ phone = '+5574981222350'
 client = TelegramClient('sessao_bot', api_id, api_hash)
 
 async def main():
-    # Isso vai forçar o Telegram a enviar o código para o seu app agora
-    await client.send_code_request(phone)
-    print("CÓDIGO ENVIADO! DEVE CHEGAR NO SEU TELEGRAM EM SEGUNDOS.")
+    print("Iniciando pedido de código oficial...")
+    # Comando oficial para forçar o envio por SMS caso não esteja logado em outro app
+    result = await client.send_code_request(phone, force_sms=True)
+    print("MENSAGEM ENVIADA! Verifique suas mensagens SMS no chip do celular.")
 
 with client:
     client.loop.run_until_complete(main())
