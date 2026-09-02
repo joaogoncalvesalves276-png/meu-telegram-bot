a9c6f91cab1cd0c2c49b93c0259256aa65f87788
joaogoncalvesalves276-png <joaogoncalvesalves276@gmail.com>
2026-09-02 16:39:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import os
 import asyncio
+import threading
+from flask import Flask
 from telethon import TelegramClient
-from telethon.errors import SessionPasswordNeededError
 
-api_id = 26553805
-api_hash = '8ec7d031bf3bb7904ba01e149ef7b629'
+app = Flask('')
+@app.route('/')
+def home(): return "Bot vivo!"
+
+def run_web_server():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+# Seus dados oficiais e novos de agora
+API_ID = 37153836
+API_HASH = "b13ec4b1dbc5f9feee1a94a67940e2"
 phone = '+5574981222350'
 
-client = TelegramClient('sessao_bot', api_id, api_hash)
+client = TelegramClient('sessao_celular', API_ID, API_HASH)
 
 async def main():
-    print("Iniciando pedido de código oficial...")
-    # Comando oficial para forçar o envio por SMS caso não esteja logado em outro app
-    result = await client.send_code_request(phone, force_sms=True)
-    print("MENSAGEM ENVIADA! Verifique suas mensagens SMS no chip do celular.")
+    await client.connect()
+    # Força o Telegram a mandar o código de 5 dígitos correto para o seu app
+    await client.send_code_request(phone)
+    print("CÓDIGO ENVIADO COM SUCESSO! VEJA SEU APLICATIVO DO TELEGRAM.")
 
-with client:
+if __name__ == '__main__':
+    t_web = threading.Thread(target=run_web_server)
+    t_web.daemon = True
+    t_web.start()
+    import nest_asyncio
+    nest_asyncio.apply()
     client.loop.run_until_complete(main())
     
